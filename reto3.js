aec768a183ea509116b680ec3a33746c1aade81a
Maykel Arias Torres <hanibal920915@gmail.com>
2022-12-22 15:39:08 -0300

    Adding reto 3

--- reto3.js
+++ reto3.js
@@ -4,18 +4,33 @@ const packOfGifts = ["book", "doll", "ball"];
 const reindeers = ["dasher", "dancer"];
 
 function distributeGifts(packOfGifts, reindeers) {
-  let lenRein = 0;
-  reindeers.map((rein) => {
-    lenRein += rein.length * 2;
-  });
-  let lenPack = 0;
-  packOfGifts.map((pack) => {
-    lenPack += pack.length;
-  });
-  console.log(lenRein,lenPack);
-  return Math.ceil(lenRein/lenPack)
+  return Math.trunc(
+    reindeers.reduce((acc, current) => {
+      return acc + current.length * 2;
+    }, 0) /
+      packOfGifts.reduce((acc, current) => {
+        return acc + current.length;
+      }, 0)
+  );
 }
 // el pack de regalos pesa 4 + 4 + 4 = 12
 // los renos pueden llevar (2 * 6) + (2 * 6) = 24
 // por lo tanto, Santa Claus puede entregar 2 cajas de regalos
-console.log(distributeGifts(packOfGifts, reindeers));
+console.log(distributeGifts(packOfGifts, reindeers),2);
+console.log(distributeGifts(["videogames", "console"], ["midu"]), 0);
+console.log(
+  distributeGifts(
+    ["game", "videoconsole", "computer"],
+    [
+      "midudev",
+      "pheralb",
+      "codingwithdani",
+      "carlosble",
+      "blasco",
+      "facundocapua",
+      "madeval",
+      "memxd",
+    ]
+  ),
+  5
+);
