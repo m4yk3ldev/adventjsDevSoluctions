8bde994e7fa684c44fd8a8dec91c86f1ebbaea66
Maykel Arias Torres <hanibal920915@gmail.com>
2022-12-23 11:55:28 -0300

    Adding travis

--- retos/1/reto1.js
+++ retos/1/reto1.js
@@ -1,11 +1,10 @@
- // https://adventjs.dev/es/challenges/2022/1
- 
+
 function wrapping(gifts) {
   const giftsReturn = gifts.map((item) => {
-    const asterisk = "*".repeat(item.length + 2);
-    return asterisk + "\\n*" + item + "*\\n" + asterisk;
-  });
-  return giftsReturn;
+   const asterisk = "*".repeat(item.length + 2);
+   return asterisk + "\n*" + item + "*\n" + asterisk;
+ });
+ return giftsReturn;
 }
-
+module.exports = wrapping;
 
